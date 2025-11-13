8eb68363a8397b30a4e8e12a5699c90a0007109f
Tatsu0shi <elpapasfritas657@gmail.com>
2025-11-13 01:44:05 -0300

    Challenges Version 1.0 | Not functional now

--- Pulmora_Maps/admin.py
+++ Pulmora_Maps/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Retos, UsuarioReto, Recompensa, UsuarioRecompensa
 
-# Register your models here.
+@admin.register(Retos)
+class RetosAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'tipo', 'icono', 'activo')
+    list_filter = ('tipo', 'activo')
+    search_fields = ('titulo', 'descripcion')
+    list_editable = ('activo',)
+
+@admin.register(Recompensa)
+class RecompensaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'icono', 'retos_requeridos', 'color')
+    ordering = ('retos_requeridos',)
+
+admin.site.register(UsuarioReto)
+admin.site.register(UsuarioRecompensa)
+
+#Admin data: username: Tatsu | Password: 89ha,121
